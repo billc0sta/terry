86dd200db588ef85317b0e9d3cce13cffb117394
root <root@bill.demain.cso>
2024-03-09 11:24:49 +0200

    adding basic upload functionality

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,9 +1,11 @@
 #include "utils.h"
+#include <errno.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 void write_error(const char* message, int close){
-  printf("[error]: %s, errno: %s", message, strerror(errno));
+  printf("[error]: %s, errno: %s\n", message, strerror(errno));
   if (close) {
     exit(1);
   }
